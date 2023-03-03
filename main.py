4c41ea69ca7ff3ca92b4bd138bfc049c69e69c3f
Vladimir Oblomov <vova.oblomov.1989@mail.ru>
2023-03-03 21:21:07 +0300

    научился итеррировать двумерный массив и записывать массив в документ xls

--- main.py
+++ main.py
@@ -6,19 +6,14 @@ from openpyxl.utils import column_index_from_string
 print('HI piple')
 columns_count = int(input('Ввидите ширину маирицы: ')) 
 rows_count = int(input('Ввидите высоту матрицы: '))
-#print(columns_count, rows_count)
-#print(randint(1, 100))
 
 # создаем книгу 
 wb = Workbook()
 # делаем единственный лист активным 
 ws = wb.active
 ws.title = input("Введите название страницы: ")
-
-for colum in range(columns_count):
-#    ws[f"A{colum+1}"] = colum
-    for row in range(rows_count):
-#        ws.
-#        print("\nтекущий столбик: ", colum)
-#        print("текущий строка: ", row)
-# wb.save("AZAZA.xls")
+for current_colum in range(1, columns_count+1):
+    for current_row in range(1, rows_count+1):
+        ws.cell(row=current_row, column=current_colum).value = randint(0, 101)
+        
+wb.save("AZAZA.xls") 
